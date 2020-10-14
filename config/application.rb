859b04ce4b27dddeef93c4da78da46e392043a52
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-10-14 10:41:35 +0300

    Add locale overrides for missing translations

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,15 @@
-require_relative 'boot'
+# frozen_string_literal: true
 
-require 'rails/all'
+require_relative "boot"
+
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module DecidimMainiodemo
+  # The application configuration class
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
@@ -15,5 +18,10 @@ module DecidimMainiodemo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Add the override translations to the load path
+    config.i18n.load_path += Dir[
+      Rails.root.join("config", "locales", "overrides/*.yml").to_s,
+    ]
   end
 end
